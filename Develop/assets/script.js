ba6d60a62db3e7a3330a98a0639edb9519c260f7
Ian Cunningham <35973428+icunningham7@users.noreply.github.com>
2022-08-14 14:18:14 -0600

    Add local storage get and set functions

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,25 +1,21 @@
 // DOM elements
 var currentDayEl = $('#currentDay');
 var scheduleListEl = $('#schedule');
+var scheduleEl;
+
 var dayStart = 9;
 var dayEnd = 17;
 
-
 // object for local storage
-var event = {
-    time: 0,
-    name: "",
-}
-
+var savedSchedule = JSON.parse(window.localStorage.getItem('schedule')) || [];
 
 // display the current day in the header
 function displayCurrentDate() {
     let currentDay = moment().format('dddd, MMMM Do');
     currentDayEl.text(currentDay);
 }
-
+// display the work day schedule
 function displayWorkSchedule() {
-    console.log("attempting schedule build");
     for (i = dayStart; i <= dayEnd; i++) {
         
         let scheduleHourEl = $('<div>').addClass('input-group row time-block');
@@ -34,13 +30,14 @@ function displayWorkSchedule() {
         let scheduleSaveEl = $('<div>').addClass('input-group-apend h-auto');
         let saveEl = $('<button>').attr({
             type: 'button',
-            class: 'btn save-button px-4 h-100'
+            class: 'btn save-button position-relative px-4 h-100'
         });
-        let saveIconEl = $('<i>').addClass('bi bi-save align-middle');
+        let saveIconEl = $('<i>').addClass('bi bi-save stretched-link align-middle');
 
 
         decorateTimeBlock(i, scheduleNameInputEl);
-
+        displayLocalStorage(i, scheduleNameInputEl);
+        
         scheduleTimeEl.append(timeEl);
         scheduleHourEl.append(scheduleTimeEl);
         scheduleHourEl.append(scheduleNameInputEl);
@@ -49,13 +46,13 @@ function displayWorkSchedule() {
         scheduleHourEl.append(scheduleSaveEl);
         scheduleListEl.append(scheduleHourEl);
     }
-    console.log("schedule built");
 
-
+    scheduleEl = $('#schedule .time-block');
+    // listener for event save button
+    scheduleEl.on('click', '.save-button', saveLocalStorage);
 }
 // decorate time blocks to represent past/present/future times
 function decorateTimeBlock(time, timeBlockEl) {
-    console.log("decorating time");
     let currentTime = moment().format('H');
     let timeBlockTime = moment(time, 'H').format('H');
 
@@ -69,11 +66,44 @@ function decorateTimeBlock(time, timeBlockEl) {
 };
 
 // saveLocalStorage
+function saveLocalStorage(event) {
+    let timeBlock = {
+        time: 0,
+        name: "",
+    }
+    let timeBlockSaved = $(event.target);
+
+    timeBlock.time = timeBlockSaved.parent().parent().parent().children('.input-group-prepend').children('span').attr('time');
+    timeBlock.name = timeBlockSaved.parent().parent().parent().children('input').val();
+
+    let oldTimeBlockEventIndex = savedSchedule.findIndex(
+        (oldtimeBlock) => {
+            return oldtimeBlock.time === timeBlock.time;
+    });
+    if (timeBlock.time && timeBlock.name) {
+        if (oldTimeBlockEventIndex !== -1) {
+            savedSchedule[oldTimeBlockEventIndex].name = timeBlock.name;
+        } else {
+            savedSchedule.push(timeBlock);
+        }
+            localStorage.setItem('schedule', JSON.stringify(savedSchedule));
+    };
+};
 // displayLocalStorage
+function displayLocalStorage(time, timeBlockEl) {
+    let scheduleTime = time;
+    console.log("running display local storage");
+    let timeBlockIndex = savedSchedule.findIndex((timeBlock) => {
+        console.log(timeBlock);
+        console.log(scheduleTime);
+        return parseInt(scheduleTime) === parseInt(timeBlock.time);
+        });
+    if (timeBlockIndex !== -1) {
+        timeBlockEl.val(savedSchedule[timeBlockIndex].name);
+    }
+}
 
 
-// listener for event save button
-// scheduleEl.on('click', '.save-button', saveLocalStorage);
 
 displayCurrentDate();
 displayWorkSchedule();
